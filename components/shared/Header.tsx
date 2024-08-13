50cf6cb582558284067822c89f3f2d31c7558be8
xyvie lyons <xyvielyons@gmail.com>
2024-08-13 17:53:42 +0300

    header component ui and functionality

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Image from 'next/image'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 const Header = () => {
   return (
     <header className='w-full border-b lg:pl-2 lg:pr-2'>
@@ -10,10 +12,15 @@ const Header = () => {
         <Link href="/" className='w-36'>
           <Image src="/assets/images/logo.svg" alt='logo' width={128} height={38}/>
         </Link>
-
+        <SignedIn>
+          <nav className='md:flex-between hidden w-full max-w-xs'>
+            <NavItems></NavItems>
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSwitchSessionUrl='/'></UserButton>
+            <MobileNav></MobileNav>
           </SignedIn>
           <SignedOut>
             <Button asChild className='rounded-full' size="lg">
