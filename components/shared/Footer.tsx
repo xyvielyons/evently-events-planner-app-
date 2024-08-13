4070c642c691b76ed57446cd681bef21a4633275
xyvie lyons <xyvielyons@gmail.com>
2024-08-13 17:58:46 +0300

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
-
+import Image from 'next/image'
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image src="/assets/images/logo.svg" alt="logo" width={128} height={38}></Image>
+        </Link>
+
+        <p>2024-2025 Evently. All Rights reserved</p>
+      </div>
+
+    </footer>
   )
 }
 
